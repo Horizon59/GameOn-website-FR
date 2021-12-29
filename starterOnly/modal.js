037a9346db27ce29460ae9e124e7cc2bf14f419a
pascaldelvallee <pdelvallee@outlook.com>
2021-12-29 18:40:46 +0100

    Validation champs et fermeture modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,92 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// regex
+
+//If (formData.querySelector(".first").value 
+
+// validate 
+function validate() {
+ const first = document.querySelector("#first").value ;
+ const last = document.querySelector("#last").value ;
+ const email = document.querySelector("#email").value ;
+ const quantity = document.querySelector("#quantity").value ;
+ const locations = document.querySelectorAll('input[name="location"]');
+ const cgu = document.querySelector("#checkbox1");
+ let hasError = false;
+
+ if (checkNames(first) === false) {
+  hasError = true;
+ }
+
+ if (checkNames(last) === false) {
+  hasError = true;
+ }
+
+
+ if (checkEmail(email) === false) {
+     hasError = true;
+ }
+ if (checkQuantity(quantity) === false) {
+     hasError = true;
+ }
+
+ if (checkLocation(locations) === false){
+     hasError = true;
+ }
+
+ if (checkCgu(cgu) === false){
+     hasError = true;
+ }
+
+
+ if(false === hasError) {
+   alert ('Pas d\'ereur');
+ } else {
+  alert ('Erreur');
+ }
+
+}
+
+function checkNames(name){
+  return name.length >= 2 && name !== "";
+}
+function checkQuantity(quantity){
+  return Number.isInteger(parseInt(quantity));
+}
+
+function checkEmail(email){
+    return  email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g) !== null;
+}
+
+function checkLocation(locations){
+    let oneChecked = false;
+
+    locations.forEach((input) => {
+        if (input.checked === true) {
+            oneChecked = true;
+        }
+    });
+
+    return oneChecked === true;
+}
+
+function checkCgu(cgu){
+    return cgu.checked === true;
+}
