048b401417039e969ee2337199d7158a092ae343
Horizon59 <88268260+Horizon59@users.noreply.github.com>
2022-01-09 19:07:58 +0100

    #validation(4)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,92 +10,96 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
+const submitBtn = document.querySelectorAll(".btn-submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+
 // launch modal form
 function launchModal() {
+  document.getElementById('first_warning').textContent= '';
   modalbg.style.display = "block";
 }
 
 // close modal form
-
 function closeModal() {
-  modalbg.style.display = "none";
+  modalbg.style.display="none";
 }
 
-// regex
 
-//If (formData.querySelector(".first").value 
-
-// validate 
 function validate() {
- const first = document.querySelector("#first").value ;
- const last = document.querySelector("#last").value ;
- const email = document.querySelector("#email").value ;
- const quantity = document.querySelector("#quantity").value ;
- const locations = document.querySelectorAll('input[name="location"]');
- const cgu = document.querySelector("#checkbox1");
- let hasError = false;
+    const firstName = document.querySelector("#first").value;
+    const lastName = document.querySelector("#last").value;
+    const email = document.querySelector("#email").value;
+    const quantity = document.querySelector("#quantity").value;
+    const locations = document.querySelectorAll('input[name="location"]:checked');
+    const cgu = document.querySelector('#checkbox1');
+    const birthdate = document.querySelector("#birthdate").value;
 
- if (checkNames(first) === false) {
-  hasError = true;
- }
+    let formIsValid = true;
 
- if (checkNames(last) === false) {
-  hasError = true;
- }
+    if (firstName.length <= 2 || firstName.value === '') {
+        document.getElementById('first_warning').textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom';
+        formIsValid = false;
+    } else {
+        document.getElementById('first_warning').textContent = '';
+    }
 
+    if (lastName.length <= 2 || lastName.value === '') {
+        document.getElementById('last_warning').textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du nom';
+        formIsValid = false;
+    } else {
+        document.getElementById('last_warning').textContent = '';
+    }
 
- if (checkEmail(email) === false) {
-     hasError = true;
- }
- if (checkQuantity(quantity) === false) {
-     hasError = true;
- }
+    if (email.match(/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/g) === null) {
+        document.getElementById('email_warning').textContent = 'Veuillez entrer une adresse mail valide';
+        formIsValid = false;
+    } else {
+        document.getElementById('email_warning').textContent = '';
+    }
 
- if (checkLocation(locations) === false){
-     hasError = true;
- }
+    if (Number.isInteger(parseInt(quantity)) === false) {
+        document.getElementById('quantity_warning').textContent = 'Veuillez saisir une valeur numérique';
+        formIsValid = false;
+    } else {
+        document.getElementById('quantity_warning').textContent = '';
+    }
 
- if (checkCgu(cgu) === false){
-     hasError = true;
- }
+    if (birthdate.match(/^[12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/g) === null){
+        document.getElementById('birthdate_warning').textContent = 'Veuillez entrer une date valide';
+        formIsValid = false;
+    } else {
+        document.getElementById('birthdate_warning').textContent = '';
+    }
 
+    if (locations.length === 0){
+        document.getElementById('location_warning').textContent = 'Veuillez choisir une option.'
+        formIsValid = false;
+    } else {
+        document.getElementById('location_warning').textContent = '';
+    }
 
- if(false === hasError) {
-   alert ('Pas d\'ereur');
- } else {
-  alert ('Erreur');
- }
+    if (cgu.checked === false){
+        document.getElementById('cgu_warning').textContent = 'Veuillez vérifier que vous acceptez les termes et conditions';
+        formIsValid = false;
+    } else {
+        document.getElementById('cgu_warning').textContent = '';
+    }
 
+    if (formIsValid === true) {
+        alert('Merci ! Votre réservation a été reçue.')
+    }
+
+    return formIsValid;
 }
 
-function checkNames(name){
-  return name.length >= 2 && name !== "";
-}
-function checkQuantity(quantity){
-  return Number.isInteger(parseInt(quantity));
-}
 
-function checkEmail(email){
-    return  email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g) !== null;
-}
+// document.getElementById('first_warning').innerHTML =
+// firstName.lenght <= 2 || "" ? '' : 'Veuillez entrer 2 caractères ou plus pour le champ du nom';
 
-function checkLocation(locations){
-    let oneChecked = false;
-
-    locations.forEach((input) => {
-        if (input.checked === true) {
-            oneChecked = true;
-        }
-    });
-
-    return oneChecked === true;
-}
-
-function checkCgu(cgu){
-    return cgu.checked === true;
-}
